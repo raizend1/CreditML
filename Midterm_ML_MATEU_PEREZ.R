3a4f2d59ffafc919347da41d74e5d84a81840d88
raizend1 <pacogppl@hotmail.com>
2017-04-04 16:37:42 +0200

    Add files via upload

---
+++ Midterm_ML_MATEU_PEREZ.R
@@ -0,0 +1,16 @@
+#remove old objects for safety resons
+rm(list=ls(all=TRUE))
+dev.off()
+
+set.seed(123)
+
+#utility function
+glue<-function(...){paste(...,sep="")}
+
+#set initial wd
+workingDir <- "D:/Documents/MIRI/Semestre 3/Machine Learning/L06/code"
+#workingDir <-"E:/Documents/Mis Documentos/MIRI/Semestre 3/Machine Learning/L06/code"
+dataDir<-glue(workingDir,"/data")
+plotDir<-glue(workingDir,"/plots")
+codeDir<-glue(workingDir,"/code")
+setwd(workingDir)
