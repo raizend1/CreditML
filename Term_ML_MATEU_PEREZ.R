aab8bf60e4ab77c2141a0c6fc272628f021a6c3b
Francisco Perez <pacogppl@hotmail.com>
2017-04-18 14:42:51 +0200

    Paths, some prior analysis

--- Term_ML_MATEU_PEREZ.R
+++ Term_ML_MATEU_PEREZ.R
@@ -1,6 +1,6 @@
 ####################################################################################################
 # Machine Learning - MIRI Master
-# Lecturer: Lluıs A. Belanche, belanche@cs.upc.edu
+# Lecturer: Lluis A. Belanche, belanche@cs.upc.edu
 # Term project
 #
 # This script represents XXXXXXXXXXXXXXXXXXXXXXXX
@@ -8,7 +8,7 @@
 # Date: 
 # Cesc Mateu
 # cesc.mateu@gmail.com
-# Francisco Pèrez
+# Francisco Perez
 # pacogppl@gmail.com
 #####################################################################################################
 
@@ -22,7 +22,12 @@ set.seed(123)
 glue<-function(...){paste(...,sep="")}
 
 #get absolute paths to work
-source("workingDir.R")
+path <- "D:/Documents/MIRI/Semestre 3/Machine Learning/TermProject"
+#path <-"E:/Documents/Mis Documentos/MIRI/Semestre 3/Machine Learning/TermProject"
+dataDir<-glue(path,"/data")
+plotDir<-glue(path,"/plots")
+codeDir<-glue(path,"/code")
+setwd(codeDir)
 
 # read initial data
 data.path<-glue(dataDir,"/","default_of_credit_card_clients.csv")
@@ -30,6 +35,7 @@ default.credit.card.data<-read.table(data.path,header = TRUE,sep = ";")
 str(default.credit.card.data)
 # refer to readme to check all the data details. Data are categorical and continous. We will predict 
 # default.payment.next.month as a binary yes (1) no (0)
+#plot(default.credit.card.data)
 
 # first check N/A values
 which(is.na(default.credit.card.data),arr.ind=TRUE) #there are none
@@ -37,13 +43,29 @@ which(is.na(default.credit.card.data),arr.ind=TRUE) #there are none
 # subset of payment history to check some interesting data - maybe
 data.sub.payment.history<-default.credit.card.data[,c(7:12)]
 
-#chechk where are the N/A values
-zero.indexes<-unique(which(data2.sub == 0 | data2.sub == -2,arr.ind=TRUE)[,1])
-length(zero.indexes)
-# [1] 25939
-dim(data2[-zero.indexes,])
-dim(data2)
-# [1] 4061
+# reduce dimensionality - apply PCA
+ 
+# execute svm -  why svm? answer this on the document
+require("kernlab")
+n.rows <- nrow(default.credit.card.data)
+n.length <-ncol(default.credit.card.data)
+# execute cross validation - maybe not because of the quantity
+# use k-fold CV with k=10
+# k <- 10 
+# folds <- sample(rep(1:k, length=N), N, replace=FALSE) 
+
+# get just one third for validation, the rest to train
+default.credit.card.data.test <- default.credit.card.data[sample(1:n.rows,size = floor(n.rows*0.3),replace = FALSE),]
+default.credit.card.train <- default.credit.card.data[which(default.credit.card.data %ni% default.credit.card.data.test),]
+
+#array for the best parameters
+c.best <- c()
+epsilon.best <- c()
+polynomial.degree.best<-c()
+
+#array for computation time
+compu.time<- c()
+
 
 
 
