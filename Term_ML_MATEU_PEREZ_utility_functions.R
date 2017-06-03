f28735fe06b2bcb5f0e4288893c6abbf957035ba
Francisco Perez <pacogppl@hotmail.com>
2017-06-03 14:53:10 +0200

    Stratified sample and latex table creator added

--- Term_ML_MATEU_PEREZ_utility_functions.R
+++ Term_ML_MATEU_PEREZ_utility_functions.R
@@ -220,6 +220,110 @@ mosthighlycorrelated <- function(mydataframe,numtoreport)
   head(fm[order(abs(fm$Correlation),decreasing=TRUE),],n=numtoreport)
 }
 
+#' @title Creates stratified sample
+#'
+#' @description
+#' Function to create stratified sampling. Credits to mrdwab from https://gist.github.com/mrdwab/6424112#file-stratified-r
+#' @param df: The input data.frame
+#' @param group: A character vector of the column or columns that make up the "strata".
+#' @param size: The desired sample size. 
+#' If size is a value less than 1, a proportionate sample is taken from each stratum.
+#' If size is a single integer of 1 or more, that number of samples is taken from each stratum.
+#' If size is a vector of integers, the specified number of samples is taken for each stratum. 
+#' It is recommended that you use a named vector. For example, if you have two strata, "A" and "B", 
+#' and you wanted 5 samples from "A" and 10 from "B", you would enter size = c(A = 5, B = 10).
+#' @param select: This allows you to subset the groups in the sampling process. This is a list. 
+#' For instance, if your group variable was "Group", and it contained three strata, "A", "B", and "C", 
+#' but you only wanted to sample from "A" and "C", you can use select = list(Group = c("A", "C")).
+#' @param replace: Boolean. For sampling with replacement.
+#' 
+#' @return log transformation for all the values
+#' 
+#' @examples
+
+# function to create stratified sampling. Credits to mrdwab from https://gist.github.com/mrdwab/6424112#file-stratified-r
+stratified <- function(df, group, size, select = NULL, 
+                       replace = FALSE, bothSets = FALSE) {
+  if (is.null(select)) {
+    df <- df
+  } else {
+    if (is.null(names(select))) stop("'select' must be a named list")
+    if (!all(names(select) %in% names(df)))
+      stop("Please verify your 'select' argument")
+    temp <- sapply(names(select),
+                   function(x) df[[x]] %in% select[[x]])
+    df <- df[rowSums(temp) == length(select), ]
+  }
+  df.interaction <- interaction(df[group], drop = TRUE)
+  df.table <- table(df.interaction)
+  df.split <- split(df, df.interaction)
+  if (length(size) > 1) {
+    if (length(size) != length(df.split))
+      stop("Number of groups is ", length(df.split),
+           " but number of sizes supplied is ", length(size))
+    if (is.null(names(size))) {
+      n <- setNames(size, names(df.split))
+      message(sQuote("size"), " vector entered as:\n\nsize = structure(c(",
+              paste(n, collapse = ", "), "),\n.Names = c(",
+              paste(shQuote(names(n)), collapse = ", "), ")) \n\n")
+    } else {
+      ifelse(all(names(size) %in% names(df.split)),
+             n <- size[names(df.split)],
+             stop("Named vector supplied with names ",
+                  paste(names(size), collapse = ", "),
+                  "\n but the names for the group levels are ",
+                  paste(names(df.split), collapse = ", ")))
+    }
+  } else if (size < 1) {
+    n <- round(df.table * size, digits = 0)
+  } else if (size >= 1) {
+    if (all(df.table >= size) || isTRUE(replace)) {
+      n <- setNames(rep(size, length.out = length(df.split)),
+                    names(df.split))
+    } else {
+      message(
+        "Some groups\n---",
+        paste(names(df.table[df.table < size]), collapse = ", "),
+        "---\ncontain fewer observations",
+        " than desired number of samples.\n",
+        "All observations have been returned from those groups.")
+      n <- c(sapply(df.table[df.table >= size], function(x) x = size),
+             df.table[df.table < size])
+    }
+  }
+  temp <- lapply(
+    names(df.split),
+    function(x) df.split[[x]][sample(df.table[x],
+                                     n[x], replace = replace), ])
+  set1 <- do.call("rbind", temp)
+  
+  if (isTRUE(bothSets)) {
+    set2 <- df[!rownames(df) %in% rownames(set1), ]
+    list(SET1 = set1, SET2 = set2)
+  } else {
+    set1
+  }
+}
+
+#' @title Creates a latex table from the inputted dataframe
+#'
+#' @description
+#' Saves a latex table in the specified location
+#' @param df: The input data.frame
+#' @param type: the type of the table. Could be 'latex'
+#' @param caption: caption for the table
+#' @param align: array of string containing the size of the columns. If unspecified will use default values. An example
+#' to get 5 columns with the second one with 10cm wide: align.columns = c("rp{10cm}rrr")
+#' @param file: name of the output file. Must contain .tex at the end
+#' 
+#' @return log transformation for all the values
+#' 
+#' @examples
+create.latex.table<-function(df,type,caption,align,file){
+  require(xtable)
+  ifelse(is.null(align),print(xtable(df,type=type,caption=caption),file=file),print(xtable(df,type=type,caption=caption,align = align.columns),file=file))
+}
+
 ####################ORIGINAL FUNCTIONS########################################
 # Original plotting·
 # ggplot(credit, aes(x = 0, y = LIMIT_BAL)) +
