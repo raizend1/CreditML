ff6aa31873be9fa91441146ed3386316935188c1
Cesc <cesc.mateu@gmail.com>
2017-06-11 19:54:51 +0200

    Ended the initial logistic regression model

--- prediction.R
+++ prediction.R
@@ -19,9 +19,10 @@
 rm(list=ls(all=TRUE))
 dev.off()
 set.seed(123)
+
+setwd('/Users/cesc/Documents/UPC MIRI-FIB/2Q/Machine Learning/Project/CreditML_Project/code/')
 source("Term_ML_MATEU_PEREZ_utility_functions.R")
 source("workingDir.R")
-
 setwd(codeDir)
 
 # Needed libraries
@@ -77,9 +78,7 @@ for(i in 6:11){
 
 str(credit)
 summary(credit)
-
 dim(credit)
-
 
 # Introduction to the prediction section ----------------------------------
 
@@ -93,7 +92,14 @@ dim(credit)
   # 4. Random Forests
   # 5. Neural Networks
 
-
+# Before going into the first type of model, we wanted to know which is our upper error threshold.
+# Let's see how much error we would get if we'd always predict the majority class.
+table(credit$default.payment.next.month)
+# Not default     Default 
+# 23364        6636 
+6636 / (23364 + 6636) # 0.2212
+# The classifier that always predicts the majority class ('Not default') would give us a 22.12% of error.
+# This is our threshold. Any model with a higher error than this can be automatically discarded.
 
 # Test and Train separation -----------------------------------------------
 
@@ -103,11 +109,23 @@ test <- credit[-train.idx,]
 
 # 80% Train - 20% Test
 
-# Logistic Regression Model -----------------------------------------------
+# Initial Logistic Regression Model -----------------------------------------------
 
 # Let's start by fitting a Logistic Regression Model with all the variables:
 
 ( logReg <- glm (default.payment.next.month ~ ., data = train, family = binomial(link=logit)) )
+summary(logReg)
+
+# Then we try to simplify the model by eliminating the least important variables progressively 
+# using the step() algorithm which penalizes models based on the AIC value.
+
+# logReg.step <- step(logReg) # Warning: It takes a while
+summary(logReg.step)
+
+# And then refit the model with the optimized model
+
+logReg <- glm (logReg.step$formula, data = train, family = binomial(link = logit))
+summary(logReg)
 
 # We observe that the weights assigned to the different variables have different orders of magnitude, 
 # which is something not desirable. As we saw during the EDA, maybe applying logarithms to some of the
@@ -118,6 +136,7 @@ test <- credit[-train.idx,]
 # Nevertheless, we use the model to do some predictions and compute a training error. Typing 'response'
 # in the prediction function will return us the predicted probabilities, it is not 'hard-assigning'
 # the observations to a class.
+
 pred <- predict (
   object = logReg,
   newdata = train,
@@ -125,18 +144,21 @@ pred <- predict (
                 )
 # We set a probability threshold 'p' from which we will classify an observation to 'Default' 
 # or 'Not Default'.
+
 p <- 0.5
 predictions <- NULL
 predictions[pred >= p] <- 1
 predictions[pred < p] <- 0
 
 # We can compute now the confusion matrix
+
 (tab <- with(credit, table(Truth=default.payment.next.month[train.idx],Pred=predictions)))
-(error.test <- 100*(1-sum(diag(tab))/nrow(train))) # 17.76 % of training error
+(error.test <- 100*(1-sum(diag(tab))/nrow(train))) # ~17 % of training error
 
 # Test error
 
 # We execute the same process but now using the test data.
+
 pred <- predict (
   object = logReg,
   newdata = test,
@@ -145,14 +167,52 @@ pred <- predict (
 
 # We set a probability threshold 'p' from which we will classify an observation to 'Default' 
 # or 'Not Default'.
+
 p <- 0.5
 predictions <- NULL
 predictions[pred >= p] <- 1
 predictions[pred < p] <- 0
 
-# We can compute now the confusion matrix
-(tab <- with(train, table(Truth=default.payment.next.month,Pred=predictions)))
-(error.test <- 100*(1-sum(diag(tab))/nrow(test))) # 
+# We can compute now the confusion matrix for the test data.
+
+(tab <- with(test, table(Truth=default.payment.next.month,Pred=predictions)))
+(error.test <- 100*(1-sum(diag(tab))/nrow(test))) # ~ 17 % of error as well.
+
+# Training error is very similar to test error, which means that we are probably 
+# not overfitting the dataset with the model. It is lower from the threshold that we initially
+# marked, 22%.
+
+# Load some libraries to evaluate the binary classifier
+
+library(pROC)
+library(caret) 
+library(e1071)
+
+# Despite that, the classifier predicts many 'Default' costumers with 'Not default'. Let's compute
+# the precision and the accuracy of the model. 
+
+levels <- c('Not default', 'Default')
+truth <- test$default.payment.next.month
+predictions <- as.factor(predictions)
+levels(predictions) <- c('Not default', 'Default')
+
+confusionMatrix(table(truth, predictions))
+
+# Plot the ROC curve
+credit$prob <- pred
+g <- roc(default.payment.next.month ~ prob, data = credit)
+plot(g)
+
+# Not very good results...
+
+# We saw during the EDA that some of the variables had very skewed distributions. The application
+# of logarithms could help improve our prediction.
+
+
+# Logistic Regression Model v2 ---------------------------------
+
+
+
 
 
 
